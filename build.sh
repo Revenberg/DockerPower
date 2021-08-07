fa8bffa7ee70c5616178dd0480a715758ecc032f
Sander Revenberg <sander@revenberg.info>
2021-08-07 16:47:05 +0200

    .

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 # version 2021-08-07 15:20
 
-cd dockerpower;git pull;cd -
+cd powercontainers;git pull;cd -
 
-cp /home/pi/dockerpower/p1meter.sh /home/pi/p1meter.sh
+cp /home/pi/powercontainers/p1meter.sh /home/pi/p1meter.sh
 chmod +x /home/pi/p1meter.sh
 
-cp /home/pi/dockerpower/build.sh /home/pi/build.sh
+cp /home/pi/powercontainers/build.sh /home/pi/build.sh
 chmod +x /home/pi/build.sh
 
 . ~/dockersolarrs485logger/build.sh
