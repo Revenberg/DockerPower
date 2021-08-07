b7cd4165faf2ae9e83d25aa0fa267263d05ddf1e
Sander Revenberg <sander@revenberg.info>
2021-08-07 13:00:01 +0200

    .

--- build.sh
+++ build.sh
@@ -6,30 +6,20 @@ cd ~
 git clone https://github.com/Revenberg/DockerPower.git 
 cd ~;cd DockerPower;git pull;cd ~
 
-git clone https://github.com/Revenberg/DockerSolarrs485logger.git 
-cd ~;cd DockerSolarrs485logger;git pull
-. ./build.sh
-cd ~
+cd ~;git clone https://github.com/Revenberg/DockerSolarrs485logger.git 
+. ~/DockerSolarrs485logger/build.sh
 
-git clone https://github.com/Revenberg/DockerWatermeter.git 
-cd ~;cd DockerWatermeter;git pull
-. ./build.sh
-cd ~
+cd ~;git clone https://github.com/Revenberg/DockerWatermeter.git 
+. ~/DockerWatermeter/build.sh
 
-git clone https://github.com/Revenberg/DockerOpenweathermap.git 
-cd ~;cd DockerOpenweathermap;git pull
-. ./build.sh
-cd ~
+cd ~;git clone https://github.com/Revenberg/DockerOpenweathermap.git 
+. ~/DockerOpenweathermap/build.sh
 
-git clone https://github.com/Revenberg/DockerP1logger.git 
-cd ~;cd DockerP1logger;git pull
-. ./build.sh
-cd ~
+cd ~;git clone https://github.com/Revenberg/DockerP1logger.git 
+. ~/DockerP1logger/build.sh
 
-git clone https://github.com/revenberg/DockerGrafana.git 
-cd ~;cd DockerGrafana;git pull
-. ./build.sh
-cd ~
+cd ~;git clone https://github.com/revenberg/DockerGrafana.git 
+. ~/DockerGrafana/build.sh
 
 cp /home/pi/DockerPower/p1meter.sh /home/pi/p1meter.sh
 chmod +x /home/pi/p1meter.sh
