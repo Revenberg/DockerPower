162b828896095e67b9c40b0ee55edfa973ef3394
Sander Revenberg <sander@revenberg.info>
2021-08-07 17:35:32 +0200

    .

--- build.sh
+++ build.sh
@@ -10,10 +10,10 @@ chmod +x /home/pi/p1meter.sh
 cp /home/pi/powercontainers/build.sh /home/pi/build.sh
 chmod +x /home/pi/build.sh
 
-. ~/dockersolarrs485logger/build.sh
-. ~/dockerwatermeter/build.sh
-. ~/dockeropenweathermap/build.sh
-. ~/dockerp1logger/build.sh
-. ~/dockergrafana/build.sh
+. ~/dockersolarrs485logger/build.sh true
+. ~/dockerwatermeter/build.sh true
+. ~/dockeropenweathermap/build.sh true
+. ~/dockerp1logger/build.sh true
+. ~/dockergrafana/build.sh true
 
 . ~/p1meter.sh
