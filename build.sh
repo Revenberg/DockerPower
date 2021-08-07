80f84c3cd070c6e95d0a05bbf7d4196f0b5b05e6
Sander Revenberg <sander@revenberg.info>
2021-08-07 15:32:42 +0200

    .

--- build.sh
+++ build.sh
@@ -2,23 +2,10 @@
 
 # version 2021-08-07 15:20
 
-cd ~
-git clone https://github.com/Revenberg/dockerpower.git 
-cd ~;cd dockerpower;git pull;cd ~
-
-cd ~;git clone https://github.com/Revenberg/dockersolarrs485logger.git 
 . ~/dockersolarrs485logger/build.sh
-
-cd ~;git clone https://github.com/Revenberg/dockerwatermeter.git 
 . ~/dockerwatermeter/build.sh
-
-cd ~;git clone https://github.com/Revenberg/dockeropenweathermap.git 
 . ~/dockeropenweathermap/build.sh
-
-cd ~;git clone https://github.com/Revenberg/dockerp1logger.git 
 . ~/dockerp1logger/build.sh
-
-cd ~;git clone https://github.com/revenberg/dockergrafana.git 
 . ~/dockergrafana/build.sh
 
 cp /home/pi/dockerpower/p1meter.sh /home/pi/p1meter.sh
