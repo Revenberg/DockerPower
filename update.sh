7edca16f217f60f8bf012ddfd52703b39173f736
Sander Revenberg <sander@revenberg.info>
2022-04-30 08:10:09 +0200

    .

--- update.sh
+++ update.sh
@@ -5,19 +5,10 @@ cd ~/powercontainers
 git pull
 cd ~
 
-cp /home/pi/powercontainers/alles.sh /home/pi/alles.sh;
-chmod +x /home/pi/alles.sh;
-
-cp /home/pi/powercontainers/build.sh /home/pi/build.sh;
-chmod +x /home/pi/build.sh;
-
-cp /home/pi/powercontainers/build.sh /home/pi/run.sh;
-chmod +x /home/pi/run.sh;
-
 cp /home/pi/powercontainers/update.sh /home/pi/update.sh;
 chmod +x /home/pi/update.sh;
 
-ansible-playbook  /home/pi/powercontainers/alles.yml --vault-password-file /home/pi/.pswrd -i /home/pi/ansible/hosts | tee /home/pi/alles.log
+ansible-playbook  /home/pi/powercontainers/update.yml --vault-password-file /home/pi/.pswrd -i /home/pi/ansible/hosts | tee /home/pi/alles.log
 
 ret=$?
 if [ $ret -ne 0 ]; then
